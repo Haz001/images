c1b25d759ecf938009bae50b8645108b7d2675fa
Haz_01 <haz01@hazonline.co.uk>
2020-01-22 13:28:26 +0000

    new interface almost done
    added a database. Download list works. Most pictures have sensible names, not just numbers.
    just need to add download feature and a selected indicator.
    After that I'll merge into master.

---
+++ db.js
@@ -0,0 +1,26 @@
+imglst = [
+{num:0,name:"Moss<br/>Burrator Reservoir",keywords:["moss","burraton","reservoir"],webpath:"img/web/img001.jpg",original:"img/Moss - Burrator Reservoir.jpg"},
+{num:1,name:"Rock<br/>Burrator Reservoir",keywords:["rock","burraton","reservoir"],webpath:"img/web/img002.jpg",original:"img/Rock - Burrator Reservoir.jpg"},
+{num:2,name:"Burrator Reservoir",keywords:["burraton","reservoir"],webpath:"img/web/img003.jpg",original:"img/Burrator Reservoir.jpg"},
+{num:3,name:"Canal<br/>The Barge Inn",keywords:["canal","barge","inn"],webpath:"img/web/img004.jpg",original:"img/Ivy Leaves - Plym Bridge Woods.jpg"},
+{num:4,name:"Ivy Leaves<br/>Plym Bridge Woods",keywords:["Ivy","Plym","Bridge","woods"],webpath:"img/web/img005.jpg",original:"img/Ivy Leaves - Plym Bridge Woods.jpg"},
+{num:5,name:"Rusty Bridge<br/>Camel Trail",keywords:["Rusty","bridge","Camel","trail"],webpath:"img/web/img006.jpg",original:"img/Rusty Bridge - Camel Trail.jpg"},
+{num:6,name:"Moss And Leaves<br/>Plym Bridge Woods",keywords:["Moss","leaves","plym","bridge","woods"],webpath:"img/web/img007.jpg",original:"img/Moss And Leaves - Plym Bridge Woods.jpg"},
+{num:7,name:"Rocks and Moss<br/>Seaton Beach",keywords:["moss","rocks","Seaton","Beach"],webpath:"img/web/img008.jpg",original:"img/Rocks and Moss - Seaton Beach.jpg"},
+{num:8,name:"Rocks<br/>Seaton Beach",keywords:["rocks","Seaton","Beach"],webpath:"img/web/img009.jpg",original:"img/Rocks - Seaton Beach.jpg"},
+{num:9,name:"Barnacles 0 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img00a.jpg",original:"img/Barnacles 0 - Seaton Beach.jpg"},
+{num:10,name:"Sea snails and shells 0 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00b.jpg",original:"img/Sea snails and shells 0 - Seaton Beach.jpg"},
+{num:11,name:"Sea snails and shells 1 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00d.jpg",original:"img/Sea snails and shells 1 - Seaton Beach.jpg"},
+{num:12,name:"Sea snails and shells 2 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00c.jpg",original:"img/Sea snails and shells 2 - Seaton Beach.jpg"},
+{num:13,name:"Sea snails and shells 3 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00e.jpg",original:"img/Sea snails and shells 3 - Seaton Beach.jpg"},
+{num:14,name:"Sea snails and shells 4 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00f.jpg",original:"img/Sea snails and shells 4 - Seaton Beach.jpg"},
+{num:15,name:"Computer Insides <br/>UK",keywords:["computer","insides","uk"],webpath:"img/web/img010-r.jpg",original:"img/Computer Insides - UK.jpg"},
+{num:16,name:"Barnacles 1 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img011-r.jpg",original:"img/Barnacles 1 - Seaton Beach.jpg"},
+{num:17,name:"Sunset <br/>UK",keywords:["Sunset","UK"],webpath:"img/web/img012-r.jpg",original:"img/Sunset - UK.jpg"},
+{num:18,name:"Santa xmas Gift<br/>Saltash, Cornwall",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img013-r.jpg",original:"img/Santa xmas Gift - Saltash, Cornwall.jpg"}
+]
+
+/*
+Santa xmas Gift - Saltash, Cornwall.jpg
+{num:17,name:"Barnacles 1 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img011.jpg",original:"img/Barnacles 1 - Seaton Beach.jpg"}
+*/
