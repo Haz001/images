12771a09c8adb0f458ff381ea3940cfac27fae01
Haz_01 <haz01@hazonline.co.uk>
2020-01-27 12:25:24 +0000

    Almost time to merge
    Download now works,
    Original link database had been adapted to work.

--- db.js
+++ db.js
@@ -1,32 +1,32 @@
 imglst = [
-{num:0,name:"Moss<br/>Burrator Reservoir",keywords:["moss","burraton","reservoir"],webpath:"img/web/img001.jpg",original:"img/Moss - Burrator Reservoir.jpg"},
-{num:1,name:"Rock<br/>Burrator Reservoir",keywords:["rock","burraton","reservoir"],webpath:"img/web/img002.jpg",original:"img/Rock - Burrator Reservoir.jpg"},
-{num:2,name:"Burrator Reservoir",keywords:["burraton","reservoir"],webpath:"img/web/img003.jpg",original:"img/Burrator Reservoir.jpg"},
-{num:3,name:"Canal<br/>The Barge Inn",keywords:["canal","barge","inn"],webpath:"img/web/img004.jpg",original:"img/Ivy Leaves - Plym Bridge Woods.jpg"},
-{num:4,name:"Ivy Leaves<br/>Plym Bridge Woods",keywords:["Ivy","Plym","Bridge","woods"],webpath:"img/web/img005.jpg",original:"img/Ivy Leaves - Plym Bridge Woods.jpg"},
-{num:5,name:"Rusty Bridge<br/>Camel Trail",keywords:["Rusty","bridge","Camel","trail"],webpath:"img/web/img006.jpg",original:"img/Rusty Bridge - Camel Trail.jpg"},
-{num:6,name:"Moss And Leaves<br/>Plym Bridge Woods",keywords:["Moss","leaves","plym","bridge","woods"],webpath:"img/web/img007.jpg",original:"img/Moss And Leaves - Plym Bridge Woods.jpg"},
-{num:7,name:"Rocks and Moss<br/>Seaton Beach",keywords:["moss","rocks","Seaton","Beach"],webpath:"img/web/img008.jpg",original:"img/Rocks and Moss - Seaton Beach.jpg"},
-{num:8,name:"Rocks<br/>Seaton Beach",keywords:["rocks","Seaton","Beach"],webpath:"img/web/img009.jpg",original:"img/Rocks - Seaton Beach.jpg"},
-{num:9,name:"Barnacles 0 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img00a.jpg",original:"img/Barnacles 0 - Seaton Beach.jpg"},
-{num:10,name:"Sea snails and shells 0 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00b.jpg",original:"img/Sea snails and shells 0 - Seaton Beach.jpg"},
-{num:11,name:"Sea snails and shells 1 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00d.jpg",original:"img/Sea snails and shells 1 - Seaton Beach.jpg"},
-{num:12,name:"Sea snails and shells 2 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00c.jpg",original:"img/Sea snails and shells 2 - Seaton Beach.jpg"},
-{num:13,name:"Sea snails and shells 3 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00e.jpg",original:"img/Sea snails and shells 3 - Seaton Beach.jpg"},
-{num:14,name:"Sea snails and shells 4 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00f.jpg",original:"img/Sea snails and shells 4 - Seaton Beach.jpg"},
-{num:15,name:"Computer Insides <br/>UK",keywords:["computer","insides","uk"],webpath:"img/web/img010-r.jpg",original:"img/Computer Insides - UK.jpg"},
-{num:16,name:"Barnacles 1 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img011-r.jpg",original:"img/Barnacles 1 - Seaton Beach.jpg"},
-{num:17,name:"Sunset <br/>UK",keywords:["Sunset","UK"],webpath:"img/web/img012-r.jpg",original:"img/Sunset - UK.jpg"},
-{num:18,name:"Santa xmas Gift<br/>Saltash, Cornwall",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img013-r.jpg",original:"img/Santa xmas Gift - Saltash, Cornwall.jpg"},
-{num:19,name:"Snow and Ruler<br/>UK",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img014-r.jpg",original:"img/Snow and Ruler - UK.jpg"},
-{num:20,name:"Calcite Crystal<br/>Rutherford Appleton<br/>Laboratory",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img015-r.jpg",original:"img/Calcite Crystal - Rutherford Appleton Laboratory.jpg"},
-{num:21,name:"Rain Drops<br/>Andover",keywords:["Rain","drops","andover"],webpath:"img/web/img016-r.jpg",original:"img/Rain Drops - Andover.jpg"},
-{num:22,name:"Bridge and Plants<br/>Mayfly Pub",keywords:["bridge","plants","river","mayfly","pub"],webpath:"img/web/img017-r.jpg",original:"img/Bridge and Plants - Mayfly Pub.jpg"}
+{num:0,name:"Moss<br/>Burrator Reservoir",keywords:["moss","burraton","reservoir"],webpath:"img/web/img001.jpg",original:"img/Moss%20%2D%20Burrator%20Reservoir.jpg"},
+{num:1,name:"Rock<br/>Burrator Reservoir",keywords:["rock","burraton","reservoir"],webpath:"img/web/img002.jpg",original:"img/Rock%20%2D%20Burrator%20Reservoir.jpg"},
+{num:2,name:"Burrator Reservoir",keywords:["burraton","reservoir"],webpath:"img/web/img003.jpg",original:"img/Burrator%20Reservoir.jpg"},
+{num:3,name:"Canal<br/>The Barge Inn",keywords:["canal","barge","inn"],webpath:"img/web/img004.jpg",original:"img/Ivy%20Leaves%20%2D%20Plym%20Bridge%20Woods.jpg"},
+{num:4,name:"Ivy Leaves<br/>Plym Bridge Woods",keywords:["Ivy","Plym","Bridge","woods"],webpath:"img/web/img005.jpg",original:"img/Ivy%20Leaves%20%2D%20Plym%20Bridge%20Woods.jpg"},
+{num:5,name:"Rusty Bridge<br/>Camel Trail",keywords:["Rusty","bridge","Camel","trail"],webpath:"img/web/img006.jpg",original:"img/Rusty%20Bridge%20%2D%20Camel%20Trail.jpg"},
+{num:6,name:"Moss And Leaves<br/>Plym Bridge Woods",keywords:["Moss","leaves","plym","bridge","woods"],webpath:"img/web/img007.jpg",original:"img/Moss%20And%20Leaves%20%2D%20Plym%20Bridge%20Woods.jpg"},
+{num:7,name:"Rocks and Moss<br/>Seaton Beach",keywords:["moss","rocks","Seaton","Beach"],webpath:"img/web/img008.jpg",original:"img/Rocks%20and%20Moss%20%2D%20Seaton%20Beach.jpg"},
+{num:8,name:"Rocks<br/>Seaton Beach",keywords:["rocks","Seaton","Beach"],webpath:"img/web/img009.jpg",original:"img/Rocks%20%2D%20Seaton%20Beach.jpg"},
+{num:9,name:"Barnacles 0 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img00a.jpg",original:"img/Barnacles%200%20%2D%20Seaton%20Beach.jpg"},
+{num:10,name:"Sea snails and shells 0 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00b.jpg",original:"img/Sea%20snails%20and%20shells%200%20%2D%20Seaton%20Beach.jpg"},
+{num:11,name:"Sea snails and shells 1 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00d.jpg",original:"img/Sea%20snails%20and%20shells%201%20%2D%20Seaton%20Beach.jpg"},
+{num:12,name:"Sea snails and shells 2 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00c.jpg",original:"img/Sea%20snails%20and%20shells%202%20%2D%20Seaton%20Beach.jpg"},
+{num:13,name:"Sea snails and shells 3 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00e.jpg",original:"img/Sea%20snails%20and%20shells%203%20%2D%20Seaton%20Beach.jpg"},
+{num:14,name:"Sea snails and shells 4 <br/>Seaton Beach",keywords:["Sea","snails","shells","Seaton","Beach"],webpath:"img/web/img00f.jpg",original:"img/Sea%20snails%20and%20shells%204%20%2D%20Seaton%20Beach.jpg"},
+{num:15,name:"Computer Insides <br/>UK",keywords:["computer","insides","uk"],webpath:"img/web/img010%2Dr.jpg",original:"img/Computer%20Insides%20%2D%20UK.jpg"},
+{num:16,name:"Barnacles 1 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img011%2Dr.jpg",original:"img/Barnacles%201%20%2D%20Seaton%20Beach.jpg"},
+{num:17,name:"Sunset <br/>UK",keywords:["Sunset","UK"],webpath:"img/web/img012%2Dr.jpg",original:"img/Sunset%20%2D%20UK.jpg"},
+{num:18,name:"Santa xmas Gift<br/>Saltash, Cornwall",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img013%2Dr.jpg",original:"img/Santa%20xmas%20Gift%20%2D%20Saltash,%20Cornwall.jpg"},
+{num:19,name:"Snow and Ruler<br/>UK",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img014%2Dr.jpg",original:"img/Snow%20and%20Ruler%20%2D%20UK.jpg"},
+{num:20,name:"Calcite Crystal<br/>Rutherford Appleton<br/>Laboratory",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img015%2Dr.jpg",original:"img/Calcite%20Crystal%20%2D%20Rutherford%20Appleton%20Laboratory.jpg"},
+{num:21,name:"Rain Drops<br/>Andover",keywords:["Rain","drops","andover"],webpath:"img/web/img016%2Dr.jpg",original:"img/Rain%20Drops%20%2D%20Andover.jpg"},
+{num:22,name:"Bridge and Plants<br/>Mayfly Pub",keywords:["bridge","plants","river","mayfly","pub"],webpath:"img/web/img017%2Dr.jpg",original:"img/Bridge%20and%20Plants%20%2D%20Mayfly%20Pub.jpg"}
 ]
 
 /*
-Bridge and Plants - Mayfly Pub.jpg
-Calcite Crystal - Rutherford Appleton Laboratory.jpg
-Snow and Ruler - UK
-{num:17,name:"Barnacles 1 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img011.jpg",original:"img/Barnacles 1 - Seaton Beach.jpg"}
+Bridge and Plants %2D Mayfly Pub.jpg
+Calcite Crystal %2D Rutherford Appleton Laboratory.jpg
+Snow and Ruler %2D UK
+{num:17,name:"Barnacles 1 <br/>Seaton Beach",keywords:["Barnacles","Seaton","Beach"],webpath:"img/web/img011.jpg",original:"img/Barnacles 1 %2D Seaton Beach.jpg"}
 */
